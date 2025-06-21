21740b9d7c51ee6ce578436aabfa7fa49fcb7c3f
Chintan Boghara <chintansboghara@gmail.com>
2025-06-21 13:13:04 +0530

    Remove Vite configuration file for Tailwind CSS integration

--- Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "boardify-terraform-state-bucket"
-    key            = "boardify/terraform.tfstate"
-    region         = "ap-south-2"
-    dynamodb_table = "boardify-terraform-lock"              
-  }
-}
